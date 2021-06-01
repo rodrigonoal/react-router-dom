e83c70f9580397c8f384b93f9ecbac5fff7d99fb
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-01 12:58:09 -0300

    Profile

--- classe-01/src/components/Profile.js
+++ classe-01/src/components/Profile.js
@@ -1,8 +1,17 @@
+import { useHistory, Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
 export default function Profile(props) {
-    return(
-        <div>
-            <h3>Olá, bem-vinds</h3>
-            <button>Logout</button>
-        </div>
-    );
+  const history = useHistory();
+  const auth = useAuth();
+
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <button onClick={() => auth.deslogar(() => history.push("/"))}>
+        Logout
+      </button>
+      <h3>Olá, bem-vinds {auth.token.login}</h3>
+    </div>
+  );
 }
