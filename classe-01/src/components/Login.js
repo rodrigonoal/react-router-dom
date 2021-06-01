b6308bcb55d6e2d68e81c606017ecd03b00da6b0
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-01 12:57:35 -0300

    Login

--- classe-01/src/components/Login.js
+++ classe-01/src/components/Login.js
@@ -1,5 +1,18 @@
+import { useHistory } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
 export default function Login(props) {
-    return(
-        <button>Login</button>
-    );
+  const auth = useAuth();
+  const history = useHistory();
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => auth.setUser(e.target.value)} />
+      <button
+        onClick={() => auth.logar(() => history.push(`/profile/${auth.user}`))}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
